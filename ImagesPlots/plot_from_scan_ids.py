c18fe19d53537706fbbc48d670ee204473ea8765
Tiago Filipe Coelho <tiago.coelho@radboudumc.nl>
2024-07-19 14:41:05 +0200

    adding _get_middle_slices_random

--- ImagesPlots/plot_from_scan_ids.py
+++ ImagesPlots/plot_from_scan_ids.py
@@ -3,7 +3,8 @@ import os
 import numpy as np
 import math
 import SimpleITK as sitk
-from typing import Optional
+from typing import Optional, List
+import random
 from exclusion_lists import rumc_exclude_subject_ids_hbv_scan_quality
 
 list_scan_ids = rumc_exclude_subject_ids_hbv_scan_quality
@@ -11,7 +12,7 @@ sequences_wanted = ['hbv']  # the sequences wanted should have the codes present
 pathname = '/Volumes/pelvis/data/prostate-MRI/rumc/images'
 
 
-def _get_list_middle_slices(path: str, list_scan_ids: list, sequences_wanted: list) -> list:
+def _get_middle_slices_from_list(path: str, list_scan_ids: list, sequences_wanted: List[str]) -> list:
     """
     Retrieve the middle slices from the specified scans and sequences.
 
@@ -59,7 +60,56 @@ def _get_list_middle_slices(path: str, list_scan_ids: list, sequences_wanted: li
     return list_middle_slices
 
 
-def plot_from_scan_ids(path: str, scan_ids: list, sequences_wanted: list, output_folder: Optional[str] = None) -> None:
+def _get_middle_slices_random(path: str, sequences_wanted: List[str], n: Optional[int] = 36) -> List[np.ndarray]:
+    """
+    Retrieve 64 random middle slices from the specified sequences.
+
+    Args:
+        path (str): The root directory containing the patient data.
+        sequences_wanted (list): A list of sequences to consider.
+        n (Optional[int]): The number of middle slices to retrieve. Default is 64.
+
+    Returns:
+        list: A list of middle slices from the specified sequences.
+    """
+    listdir = os.listdir(path)
+    random.shuffle(listdir)
+    list_middle_slices = []
+
+
+    for patient in listdir:
+        patient_path = os.path.join(path, patient)
+        if not os.path.isdir(patient_path):
+            print(f"Skipping non-directory item: {patient_path}")
+            continue
+        for scan in os.listdir(patient_path):
+            if sequences_wanted[0] in scan:
+                scan_path = os.path.join(patient_path, scan)
+                try:
+                    # Read MHA image
+                    image = sitk.ReadImage(scan_path)
+                    # Get image array
+                    image_array = sitk.GetArrayFromImage(image)
+                    middle_slice_idx = np.shape(image_array)[0] // 2
+                    # Get middle slice and add to list
+                    list_middle_slices.append(image_array[middle_slice_idx])
+                    if len(list_middle_slices) >= n:
+                        break
+                    else:
+                        continue
+                except RuntimeError as e:
+                    print(f"Error reading image at {scan_path}: {e}")
+                    print("Skipping this image and continuing with the next one.")
+                except Exception as e:
+                    print(f"Unexpected error processing image at {scan_path}: {e}")
+
+        if len(list_middle_slices) >= n:
+            break
+
+    return list_middle_slices
+
+
+def plot_from_scan_ids(path: str, sequences_wanted: list, scan_ids: Optional[list] = None, output_folder: Optional[str] = None) -> None:
     """
     Plot middle slices from specified scans and sequences in a grid. Optionally save the plots to an output folder.
 
@@ -70,7 +120,10 @@ def plot_from_scan_ids(path: str, scan_ids: list, sequences_wanted: list, output
         output_folder (Optional[str]): The directory to save the plots. If None, plots are not saved.
     """
     # Get the list of middle slices
-    middle_slices = _get_list_middle_slices(path, scan_ids, sequences_wanted)
+    if scan_ids is None:
+        middle_slices = _get_middle_slices_random(path, sequences_wanted)
+    else:
+        middle_slices = _get_middle_slices_from_list(path, sequences_wanted, scan_ids)
 
     if not middle_slices:
         print("No slices to plot.")
@@ -106,4 +159,4 @@ def plot_from_scan_ids(path: str, scan_ids: list, sequences_wanted: list, output
         plt.show()
 
 
-plot_from_scan_ids(pathname, list_scan_ids, sequences_wanted)
+plot_from_scan_ids(pathname, sequences_wanted)
